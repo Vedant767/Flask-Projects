956e269bd60afafba77a673805f47dd127e54808
Vedant <vedantgire48@gmail.com>
2022-07-09 22:21:01 +0530

    Second commit in wheather app

--- wheather_app/app.py
+++ wheather_app/app.py
@@ -1,12 +1,13 @@
 from flask import Flask, render_template, request
 import urllib.request, json
+from ..config import api_id
 app = Flask(__name__)
 
 @app.route("/", methods = ["GET", "POST"])
 def weather():
     if request.method == "POST":
         cname = request.form.get("cname")
-        url = f"https://api.openweathermap.org/data/2.5/weather?q={cname}&appid=3a4108a926bc14a4eaf425100cb802cf"
+        url = f"https://api.openweathermap.org/data/2.5/weather?q={cname}&appid={api_id}"
         response = urllib.request.urlopen(url)
         data = response.read()
         dict = json.loads(data)
